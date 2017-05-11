a2831e7068429d433c87113416bd400b1a46a111
Jason Manns <Jason.Manns@gmx.com>
2017-05-11 22:12:01 +0100

    Display user agent info for /api/whoami

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fingerprint = require('express-fingerprint')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(fingerprint({
+  parameters: [
+    fingerprint.useragent
+  ]
+}))
+
+app.get('/api/whoami', (req, res) => {
+
+  const os = req.fingerprint.components.useragent.os.family
+  const ip = req.ip
+  const lang = req.headers['accept-language'].split(',')[0]
+  res.json({
+    ip,
+    os,
+    lang
+  })
+})
+
+app.get('*', (req, res) => {
+  res.send(`
+    <div style="height:100vh;display:flex;justify-content:center;flex-direction:column;align-items:center;">
+      <h1>FreeCodeCamp header parser </h1>
+      <p>Visit /api/whoami for client info </p>
+    </div>
+  `)
+})
+
+app.listen(port)
